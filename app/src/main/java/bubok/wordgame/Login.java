2cc44223eeb135f391aa63e686b3df309708f707
Albert <bubokastor@mail.ru>
2016-03-01 16:36:24 +0300

    Добавлена кнопка, текстовое поле. Событие на кнопку

--- app/src/main/java/bubok/wordgame/Login.java
+++ app/src/main/java/bubok/wordgame/Login.java
@@ -2,12 +2,20 @@ package bubok.wordgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    private EditText editTextlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextlogin = (EditText) findViewById(R.id.editTextLogin);
+    }
+
+    public void ButtonEnterClick(View v){
+
     }
 }
